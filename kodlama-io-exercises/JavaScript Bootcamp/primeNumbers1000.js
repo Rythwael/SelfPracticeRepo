42e0aedc704cfad191ae09e8f7f499fdaf735841
Oğuzhan <akcahanoguz@gmail.com>
2021-12-28 16:25:09 +0300

    Prime Numbers until 1000 added.

---
+++ kodlama-io-exercises/JavaScript Bootcamp/primeNumbers1000.js
@@ -0,0 +1,20 @@
+// 1000'e kadarki tüm asal sayıları listeleyen programı yazınız.
+
+function primeNumbers() {
+    let result = []
+    for (let i = 2; i < 1000; i++) {
+      isPrime = true;
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime === true) {
+        result.push(i)
+      }
+    }
+    console.log(...result)
+  }
+
+  primeNumbers()
