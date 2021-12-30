4a5e540e0fdecd5f8d162acb1f33579046c0f2b7
Oğuzhan <akcahanoguz@gmail.com>
2021-12-30 17:17:26 +0300

    http module tested

---
+++ freecodecamp-exercises/nodejs/johnSmilga8h/11-http-module.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.end('Welcome to our homepage')
+    }
+    if(req.url==='/about'){
+        res.end("Here is our short history")
+    }
+    res.end(`
+    <h1>Opps!</h1>
+    `)
+    
+})
+
+server.listen(3000)
