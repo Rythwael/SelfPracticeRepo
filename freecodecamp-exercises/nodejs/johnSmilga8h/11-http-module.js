1297df9e0d1e8f3e84540f4220705bb9d4f6d73a
Oğuzhan <akcahanoguz@gmail.com>
2021-12-30 17:17:42 +0300

    http edited

--- freecodecamp-exercises/nodejs/johnSmilga8h/11-http-module.js
+++ freecodecamp-exercises/nodejs/johnSmilga8h/11-http-module.js
@@ -1,16 +1,16 @@
-const http = require('http')
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    if(req.url==='/'){
-        res.end('Welcome to our homepage')
-    }
-    if(req.url==='/about'){
-        res.end("Here is our short history")
-    }
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our homepage");
+  }
+  if (req.url === "/about") {
+    res.end("Here is our short history");
+  } else {
     res.end(`
-    <h1>Opps!</h1>
-    `)
-    
-})
+        <h1>Opps!</h1>
+        `);
+  }
+});
 
-server.listen(3000)
+server.listen(3000);
