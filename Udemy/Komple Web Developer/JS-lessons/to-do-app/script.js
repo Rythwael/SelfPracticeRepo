a47b0f171aebdf7ee434225b74be97ef6c2e4d03
Oğuzhan <akcahanoguz@gmail.com>
2022-10-28 09:47:47 +0300

    Task edit added

--- Udemy/Komple Web Developer/JS-lessons/to-do-app/script.js
+++ Udemy/Komple Web Developer/JS-lessons/to-do-app/script.js
@@ -1,8 +1,12 @@
-let taskList = [{ id: 1, taskName: "Yalar" }];
+let taskList = [];
 let ul = document.getElementById("task-list");
 let addBtn = document.querySelector("#btnAdd");
 let clearBtn = document.querySelector("#btnClear");
 let taskCount = document.querySelector("#task-count");
+let task = document.querySelector("#task-input");
+let editId;
+let isEdit = false;
+let isAdd = false;
 let count = taskList.length + 1;
 displayTasks();
 
@@ -30,39 +34,72 @@ function displayTasks() {
 
 addBtn.addEventListener("click", function (event) {
     let isHas = false;
-    let task = document.querySelector("#task-input");
-    for (tasks of taskList) {
-        console.log(tasks.taskName);
-        if (tasks.taskName === task.value || task.value === "") {
+    if (taskList.length > 0) {
+        for (tasks of taskList) {
+            if (tasks.taskName == task.value || task.value == "") {
+                isHas = true;
+                alert("Task already used");
+                task.value = "";
+                break;
+            }
+        }
+    } else {
+        if (task.value == "") {
             isHas = true;
-            break;
         }
     }
-    if (isHas === false) {
-        taskList.push({ id: count, taskName: task.value });
-        let li = `<li class="task d-flex list-group-item">
-        <div class="d-flex form-check me-auto">
-            <span class="pt-1">
-            <input type="checkbox" id="${count}" class="form-check-input"/>
-            <label for="${count}" class="form-check-label me-auto">${task.value}
-        </div>
-        <div class="dropdown">
-            <button class="btn btn-link btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fa-solid fa-ellipsis"></i>
-            </button>
-            <ul class="dropdown-menu">
-              <li><a onclick="editTask(${count})" class="dropdown-item" href="#"><i class="fa-solid fa-pen"></i> Edit</a></li>
-              <li><a onclick="deleteTask(${count})" class="dropdown-item" href="#"><i class="fa-solid fa-trash"></i> Delete</a></li>
-            </ul>
-          </div>
-        </li>
-            `;
-        ul.insertAdjacentHTML("beforeend", li);
-        count++;
 
+    if (isEdit === false) {
+        if (isHas === false) {
+            taskList.push({ id: count, taskName: task.value });
+
+            let li = `<li class="task d-flex list-group-item">
+            <div class="d-flex form-check me-auto">
+                <span class="pt-1">
+                <input type="checkbox" id="${count}" class="form-check-input"/>
+                <label for="${count}" class="form-check-label me-auto">${task.value}
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-link btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fa-solid fa-ellipsis"></i>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a onclick='editTask(${count},"${task.value}")' class="dropdown-item" href="#"><i class="fa-solid fa-pen"></i> Edit</a></li>
+                  <li><a onclick="deleteTask(${count})" class="dropdown-item" href="#"><i class="fa-solid fa-trash"></i> Delete</a></li>
+                </ul>
+              </div>
+            </li>
+                `;
+            ul.insertAdjacentHTML("beforeend", li);
+            count++;
+            task.value = "";
+        }
     }
-    task.value = "";
-    console.log(taskList);
+    if (isEdit === true) {
+        for (tasks of taskList) {
+            if (tasks.taskName === task.value) {
+                alert("Please add a different task");
+                isAdd = false;
+                break;
+            }
+            if (task.value === "") {
+                alert("Please don't add empty task");
+                isAdd = false;
+                break;
+            }
+        }
+        if (isAdd) {
+            let taskIndex = taskList.findIndex(function (task) {
+                return task.id == editId;
+            })
+            taskList[taskIndex].taskName = task.value;
+            let editedTask = document.getElementById(editId).parentElement.children[1].innerText = task.value
+            console.log(editedTask);
+            task.value = "";
+            isEdit = false;
+            isAdd = false;
+        }
+    }
     event.preventDefault();
 });
 
@@ -71,14 +108,15 @@ clearBtn.addEventListener("click", function (event) {
         ul.removeChild(ul.firstChild);
     }
     taskList = [];
-    taskCount.innerText = taskList.length;
     event.preventDefault();
 });
 
-function editTask(id) {
-    console.log(id);
+function editTask(taskId, taskName) {
+    isEdit = true;
+    isAdd = true;
+    task.value = taskName;
+    editId = taskId;
 }
-
 
 
 function deleteTask(id) {
