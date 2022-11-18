25bce2165489b8e8a069c9c3925b2d9946d3df9f
Oğuzhan <akcahanoguz@gmail.com>
2022-11-18 14:49:34 +0300

    Request class added

--- Udemy/Modern Web Development/EmployeesWithModules/src/index.js
+++ Udemy/Modern Web Development/EmployeesWithModules/src/index.js
@@ -1,1 +1,14 @@
-console.log("Test");
+import { Request } from "./request";
+
+const form = document.querySelector("#employee-form");
+const nameInput = document.querySelector("#name");
+const departmentInput = document.querySelector("#department");
+const salaryInput = document.querySelector("#salary");
+const employeesList = document.querySelector("#employees");
+
+const request = new Request("http://localhost:3000/employees");
+
+//request.get().then(employees => console.log(employees)).catch(err => { console.log(err) });
+//request.post({ name: "Hakan Taşıyan", department: "Business", salary: 10000 }).then(employee => console.log(employee)).catch(err => console.log(err));
+//request.put(3, { name: "Hasan Şaş", department: "IT", salary: 15000 }).then(employee => console.log(employee)).catch(err => console.log(err));
+//request.delete(4).then(msg => console.log(msg)).catch(err => console.log(err))
